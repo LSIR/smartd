353eb7096af925ed5e2cf13330445c61b49cd433
Tri Kurniawan Wijaya <tri-kurniawan.wijaya@epfl.ch>
2014-05-29 14:34:13 +0200

    Adding French.

---
+++ gsn/webapp/js/smartd-languages/fr.js
@@ -0,0 +1,71 @@
+
+// Language : English
+
+var lang = {
+
+firstVisualization : "Comparaison",
+secondVisualization : "Prédiction",
+thirdVisualization : "Évaluation",
+fourthVisualization : "Distribution",
+
+firstControlPanel : "Mesure de différents ID",
+thirdControlPanel : "Distribution des valeurs",
+
+startTime : "l'heure de début",
+endTime : "l'heure de fin",
+
+interval : "Intervalle",
+everyThirtyMin : "toutes les 30 minutes",
+hourly : "horaire",
+daily : "tous les jours",
+weekly : "hebdomadaire",
+monthly : "mensuel",
+
+min : "min",
+max : "max",
+sum : "somme",
+avg : "moyenne",
+
+normalization: "Valeurs normalisées",
+id: "ID",
+displayPlot : "Affichage",
+savePlot : "Enregistrer",
+loadPlot : "Load",
+
+title1 : "Choisissez 5 attributs!",
+title2 : "Remplissez les valeurs!",
+title3 : "Évaluation",
+
+weekDay : "Jour de la semaine",
+sunday : "Dimanche",
+monday : "Lundi",
+tuesday : "Mardi",
+wednesday : "Mercredi",
+thursday : "Jeudi",
+friday : "Vendredi",
+saturday: "Samedi",
+weekdays : "semaine",
+weekend : "week-end",
+
+season : "Season",
+winter : " l'hiver (Dec-Feb)",
+spring : "printemps (Mar-May)",
+summer : "l'été (Jun-Aug)",
+autumn : "l'automne (Sep-Nov)",
+
+all : "tous",
+
+refreshPlot : "Ne pas actualiser le terrain",
+
+EvaluationOfnNeighbors : "Évaluation des n voisins",
+EvaluationOfkQuestions : "l'évaluation des questions de k",
+
+neighborNum : "Nombre de voisins",
+questionNum : "Nombre de questions",
+
+bin: "entrée bin",
+binNum : "nombre de poubelles",
+binSize : "la taille de la poubelle"	
+	
+
+}
